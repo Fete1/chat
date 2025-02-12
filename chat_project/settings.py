e72bb546aa265c2f77c4e7d4fe477e0502dc4b55
Fete1 <104610705+Fete1@users.noreply.github.com>
2025-02-12 17:50:55 +0300

    Update settings.py

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fetene12.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 
